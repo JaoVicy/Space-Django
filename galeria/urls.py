14b55a31594734670255bc08466b08b7a8e40c8d
JaoVicy <filipimjoa@gmail.com>
2025-07-28 15:39:47 -0300

    style: added imagem.html paths

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 
 urlpatterns = [
     path('', index),
+    path('imagem/', index, ),  # Assuming you want to handle 'imagem' view as well
 ]
