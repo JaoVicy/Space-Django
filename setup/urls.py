638995d870eba388502f12c3e218e9800dcb247f
JaoVicy <filipimjoa@gmail.com>
2025-07-28 14:12:57 -0300

    refactor: added urlpatterns list

--- setup/urls.py
+++ setup/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from galeria.views import index
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', index),
+    path('', include('galeria.urls')),  # Include the galeria app URLs
 ]
