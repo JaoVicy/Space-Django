47deb6d3949a0e2a4da9d245ba0c9b4208a14811
JaoVicy <filipimjoa@gmail.com>
2025-07-28 14:26:23 -0300

    test: added index in views

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to the gallery!")
+    return render(request, 'index.html')
